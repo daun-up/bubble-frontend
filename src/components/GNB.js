4fcf9c556b8a4772c2b8cce07e29f087bdbae8d4
JeongBin Park <obb8923@naver.com>
2024-09-01 03:22:31 +0900

    되냐?3

--- src/components/GNB.js
+++ src/components/GNB.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 const GNB = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -34,68 +34,7 @@ const GNB = () => {
         <div
           className="flex flex-col w-full h-full"
           style={{ padding: '1rem 0' }}
-        >
-          <div className="flex items-center m-6 gap-6">
-            <div
-              className="w-10 h-10"
-              style={{ backgroundColor: '#90CCDA', borderRadius: '25px' }}
-            ></div>
-            <span>김진성</span>
-          </div>
-          <button
-            className="w-full hover:bg-gray-100"
-            style={{
-              padding: '1rem 1.5rem',
-              borderTop: '1.5px solid #90CCDA',
-              color: '#90CCDA',
-            }}
-            onClick={() => {
-              navigate('/MyBubbles');
-            }}
-          >
-            내 경험 방울
-          </button>
-          <button
-            className="w-full hover:bg-gray-100"
-            style={{
-              padding: '1rem 1.5rem',
-              borderTop: '1.5px solid #90CCDA',
-              color: '#90CCDA',
-            }}
-            onClick={() => {
-              navigate('/ReceivedBubbles');
-            }}
-          >
-            요청받은 방울
-          </button>
-          <button
-            className="w-full hover:bg-gray-100"
-            style={{
-              padding: '1rem 1.5rem',
-              borderTop: '1.5px solid #90CCDA',
-              color: '#90CCDA',
-            }}
-            onClick={() => {
-              navigate('/RequestedBubbles');
-            }}
-          >
-            요청한 방울
-          </button>
-          <button
-            className="w-full hover:bg-gray-100"
-            style={{
-              padding: '1rem 1.5rem',
-              borderTop: '1.5px solid #90CCDA',
-              borderBottom: '1.5px solid #90CCDA',
-              color: '#90CCDA',
-            }}
-            onClick={() => {
-              navigate('/manual');
-            }}
-          >
-            사용설명
-          </button>
-        </div>
+        ></div>
       </div>
     </>
   );
