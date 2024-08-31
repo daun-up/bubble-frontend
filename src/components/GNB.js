9be5128b72b146a4e073226210af364168416ed6
JeongBin Park <obb8923@naver.com>
2024-09-01 03:20:56 +0900

    되냐?2

--- src/components/GNB.js
+++ src/components/GNB.js
@@ -11,14 +11,16 @@ const GNB = () => {
 
   return (
     <>
-      {/* 되냐? */}
+      {/* 되냐?2 */}
+      {/* 햄버거 아이콘 */}
       <div
         onClick={toggleMenu}
         className="fixed top-4 right-4 cursor-pointer z-20"
         style={{ color: '#90CCDA' }}
       >
-        <p className="text-2xl">☰</p> {/* 햄버거 아이콘 */}
+        <p className="text-2xl">☰</p>
       </div>
+      {/* 메뉴  */}
       <div
         className={`fixed bg-white right-0 z-10 transition-transform duration-300 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
         style={{
@@ -33,71 +35,66 @@ const GNB = () => {
           className="flex flex-col w-full h-full"
           style={{ padding: '1rem 0' }}
         >
-          <div
-            className="flex flex-col w-full h-full"
-            style={{ padding: '1rem 0' }}
-          >
-            <div className="flex items-center m-6 gap-6">
-              <div
-                className="w-10 h-10"
-                style={{ backgroundColor: '#90CCDA', borderRadius: '25px' }}
-              ></div>
-              <span>김진성</span>
-            </div>
-            <button
-              className="w-full hover:bg-gray-100"
-              style={{
-                padding: '1rem 1.5rem',
-                borderTop: '1.5px solid #90CCDA',
-                color: '#90CCDA',
-              }}
-              onClick={() => {
-                navigate('/MyBubbles');
-              }}
-            >
-              내 경험 방울
-            </button>
-            <button
-              className="w-full hover:bg-gray-100"
-              style={{
-                padding: '1rem 1.5rem',
-                borderTop: '1.5px solid #90CCDA',
-                color: '#90CCDA',
-              }}
-              onClick={() => {
-                navigate('/ReceivedBubbles');
-              }}
-            >
-              요청받은 방울
-            </button>
-            <button
-              className="w-full hover:bg-gray-100"
-              style={{
-                padding: '1rem 1.5rem',
-                borderTop: '1.5px solid #90CCDA',
-                color: '#90CCDA',
-              }}
-              onClick={() => {
-                navigate('/RequestedBubbles');
-              }}
-            >
-              요청한 방울
-            </button>
-            <button
-              className="w-full hover:bg-gray-100"
-              style={{
-                padding: '1rem 1.5rem',
-                borderTop: '1.5px solid #90CCDA',
-                borderBottom: '1.5px solid #90CCDA',
-                color: '#90CCDA',
-              }}
-              onClick={() => {
-                navigate('/manual');
-              }}
-            >
-              사용설명
-            </button>
+          <div className="flex items-center m-6 gap-6">
+            <div
+              className="w-10 h-10"
+              style={{ backgroundColor: '#90CCDA', borderRadius: '25px' }}
+            ></div>
+            <span>김진성</span>
           </div>
+          <button
+            className="w-full hover:bg-gray-100"
+            style={{
+              padding: '1rem 1.5rem',
+              borderTop: '1.5px solid #90CCDA',
+              color: '#90CCDA',
+            }}
+            onClick={() => {
+              navigate('/MyBubbles');
+            }}
+          >
+            내 경험 방울
+          </button>
+          <button
+            className="w-full hover:bg-gray-100"
+            style={{
+              padding: '1rem 1.5rem',
+              borderTop: '1.5px solid #90CCDA',
+              color: '#90CCDA',
+            }}
+            onClick={() => {
+              navigate('/ReceivedBubbles');
+            }}
+          >
+            요청받은 방울
+          </button>
+          <button
+            className="w-full hover:bg-gray-100"
+            style={{
+              padding: '1rem 1.5rem',
+              borderTop: '1.5px solid #90CCDA',
+              color: '#90CCDA',
+            }}
+            onClick={() => {
+              navigate('/RequestedBubbles');
+            }}
+          >
+            요청한 방울
+          </button>
+          <button
+            className="w-full hover:bg-gray-100"
+            style={{
+              padding: '1rem 1.5rem',
+              borderTop: '1.5px solid #90CCDA',
+              borderBottom: '1.5px solid #90CCDA',
+              color: '#90CCDA',
+            }}
+            onClick={() => {
+              navigate('/manual');
+            }}
+          >
+            사용설명
+          </button>
         </div>
       </div>
     </>
