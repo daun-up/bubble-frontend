916535e74b1dcc5d48a60d5b3b152c7887013c73
daun-up <awhekdns@kookmin.ac.kr>
2024-09-01 05:05:34 +0900

    feature: 글쓰기 플로우 추가

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import GNB from '../components/GNB';
 
 export default function FormPage() {
   return (
     <div className="relative flex justify-center items-center w-screen h-screen overflow-clip">
+      <div className="z-30">
+        <GNB />
+      </div>
       {/* 배경 원들 */}
-      <div className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[400px] h-[400px] top-[0%] left-[0%] z-10"></div>
-      <div className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[300px] h-[300px] bottom-[3%] left-[10%] z-10"></div>
-      <div className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[150px] h-[150px] top-[20%] right-[3%] z-10"></div>
+      <div
+        className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[400px] h-[400px] top-[0%] left-[0%] z-10"></div>
+      <div
+        className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[300px] h-[300px] bottom-[3%] left-[10%] z-10"></div>
+      <div
+        className="absolute bg-transparent border border-[#90CCDA] rounded-full w-[150px] h-[150px] top-[20%] right-[3%] z-10"></div>
 
       {/* 중앙 원 */}
-      <div className="flex flex-col justify-center items-center border-[#90CCDA] rounded-full w-[970px] h-[970px] border-2 border-solid bg-white z-20 overflow-hidden">
+      <div
+        className="flex flex-col justify-center items-center border-[#90CCDA] rounded-full w-[970px] h-[970px] border-2 border-solid bg-white z-20 overflow-hidden">
         <div className="text-center w-[550px] h-[650px] flex flex-col justify-center">
           <div>
             <input
