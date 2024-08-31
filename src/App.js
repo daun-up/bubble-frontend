145896daff47f3b29a2b900717e55580b9a41b35
daun-up <awhekdns@kookmin.ac.kr>
2024-08-31 23:02:08 +0900

    feature: menual 페이지 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import TargetPage from './pages/TargetPage'
+import ManualPage from './pages/ManualPage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path='/target' element={<TargetPage/>} />
+        <Route path="/manual" element={<ManualPage/>} />
       </Routes>
     </BrowserRouter>
   );
