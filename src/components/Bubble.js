ced34d96aa5925b9662c034f628a15cdb7895028
JeongBin Park <obb8923@naver.com>
2024-09-01 00:10:02 +0900

    버블 움직이게

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -2,8 +2,9 @@
 import BubbleUI from 'react-bubble-ui';
 import 'react-bubble-ui/dist/index.css';
 import Child from './Child';
-// import './myComponent.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+// import axios from 'axios';
+
 export default function Bubble(props) {
   const options = {
     size: 180,
@@ -11,16 +12,81 @@ export default function Bubble(props) {
     gutter: 33,
     provideProps: true,
     numCols: 6,
-    fringeWidth: 98,
-    yRadius: 130,
-    xRadius: 220,
-    cornerRadius: 50,
+    fringeWidth: 180,
+    yRadius: 180,
+    xRadius: 280,
+    cornerRadius: 150,
     showGuides: false,
     compact: true,
-    gravitation: 4,
+    gravitation: 8,
   };
 
-  const [arr] = useState(['소디패 후기', '교환학생 후기', '제목3', '','제목5']);
+  const [arr, setArr] = useState([
+    '소디패 후기',
+    '교환학생 후기',
+    '제목3',
+    'sadass',
+    '제목5',
+    '소디패 후기',
+    '교환학생 후기',
+    '제목3',
+    '',
+    '제목5',
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // let uri = '';
+      // if (window.location.pathname === '/ReceivedBubbles') {
+      //   uri = 'back/api/get1';
+      // } else if (window.location.pathname === '/RequestedBubbles') {
+      //   uri = 'back/api/get2';
+      // } else if (window.location.pathname === '/MyBubbles') {
+      //   uri = 'back/api/get3';
+      // }
+      // try {
+      //   const response = await axios.get(uri);
+      //   const data = await response.data;
+      //   setArr(data);
+      // } catch (error) {
+      //   console.error('데이터를 가져오는 중 오류 발생:', error);
+      // }
+      setArr([
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        'sadass',
+        '제목5',
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        '',
+        '제목5',
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        'sadass',
+        '제목5',
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        '',
+        '제목5',
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        'sadass',
+        '제목5',
+        '소디패 후기',
+        '교환학생 후기',
+        '제목3',
+        '',
+        '제목5',
+      ]);
+    };
+
+    fetchData();
+  }, []);
 
   const children = arr.map((data, i) => {
     return <Child data={data} className="child" key={i} />; // Self-closing tag로 수정
