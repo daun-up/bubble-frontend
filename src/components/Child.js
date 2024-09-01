45384f6f3b861b92978e935df546d4433a01afc9
daun-up <awhekdns@kookmin.ac.kr>
2024-09-01 10:36:03 +0900

    css 수정

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,59 +3,64 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Child({ data, id, defaultPath, detailPath }) {
   const navigate = useNavigate();
-  const location = useLocation(); // 현재 URL 경로를 가져오는 훅
+  const location = useLocation();
 
-  const colors = ['#ECFAFE', '#90CCDA', '#BEDBE2']; // 사용 가능한 색상들
+  const colors = ['#ECFAFE', '#90CCDA', '#BEDBE2'];
   const [backgroundColor] = useState(
     () => colors[Math.floor(Math.random() * colors.length)],
   );
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  // 배경색에 따라 글자색을 설정
-  const textColor = backgroundColor === '#ECFAFE' ? '#454C54' : '#FFFFFF';
+  const textColor = '#FFFFFF';
 
   const handleClick = () => {
-    // 현재 경로에 따라 detailPath를 유동적으로 설정
     if (location.pathname === defaultPath) {
       navigate(`${detailPath}/${id}`);
     } else {
-      navigate(`/bubble/${id}`); // 기본적으로 /bubble/{id}로 이동
+      navigate(`/bubble/${id}`);
     }
   };
 
   useEffect(() => {
-    // 컴포넌트가 마운트될 때 즉시 position을 업데이트
     setPosition({
       x: Math.floor(Math.random() * 40) - 20,
       y: Math.floor(Math.random() * 40) - 20,
     });
 
-    // 주기적으로 position을 업데이트하여 div를 움직이도록 합니다.
     const interval = setInterval(() => {
       setPosition({
         x: Math.floor(Math.random() * 40) - 20,
         y: Math.floor(Math.random() * 40) - 20,
       });
-    }, 2000); // 2000ms마다 업데이트 (2초)
+    }, 2000);
 
-    return () => clearInterval(interval); // 컴포넌트가 unmount 될 때 interval을 정리합니다.
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div
-      className="flex justify-center w-full h-full rounded-full cursor-pointer transform transition-transform duration-1000 hover:scale-125"
+      className="flex justify-center w-full h-full rounded-full cursor-pointer transform transition-transform duration-1000 hover:scale-125 relative"
       style={{
         backgroundColor,
         color: textColor,
-        transform: `translate(${position.x}px, ${position.y}px)`, // div의 위치를 설정
-        transition: 'transform 3s linear 0s', // 부드러운 움직임을 위한 transition 추가
-        backgroundImage: `url(${data.image})`, // data.image의 URL을 배경 이미지로 설정
-        backgroundSize: 'cover', // 배경 이미지를 div 크기에 맞게 조정
-        backgroundPosition: 'center', // 배경 이미지의 위치를 중앙으로 설정
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        transition: 'transform 3s linear 0s',
+        backgroundImage: `url(${data.image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
       onClick={handleClick}
     >
-      <p className="content-center">{data.title}</p>
+      {/* 불투명한 막 */}
+      <div
+        className="absolute inset-0 bg-black opacity-50 rounded-full"
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.5)', // 불투명한 검은색 막
+        }}
+      ></div>
+
+      {/* 텍스트 */}
+      <p className="relative z-10 content-center text-white">{data.title}</p>
     </div>
   );
 }
