aa6c7bee1819e9aaa21745e9f3b0e62cf10d54ac
JeongBin Park <obb8923@naver.com>
2024-09-01 09:03:38 +0900

    403?

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // useNavigate 훅 임포트
 
 export default function Bubble(props) {
+  const userId = sessionStorage.getItem('userId');
   const options = {
     size: 180,
     minSize: 20,
@@ -26,12 +27,12 @@ export default function Bubble(props) {
   useEffect(() => {
     const fetchData = async () => {
       let uri = '';
-      if (window.location.pathname === '/ReceivedBubbles') {
-        uri = 'back/api/post';
-      } else if (window.location.pathname === '/RequestedBubbles') {
-        uri = 'back/api/post';
-      } else if (window.location.pathname === '/MyBubbles') {
-        uri = 'back/api/post';
+      if (window.location.pathname === '/received-bubbles') {
+        uri = `back/api/contract/receive/${userId}`;
+      } else if (window.location.pathname === '/requested-bubbles') {
+        uri = `back/api/contract/request/${userId}`;
+      } else if (window.location.pathname === '/my-bubbles') {
+        uri = `back/api/post/${userId}/users`;
       } else if (window.location.pathname === '/') {
         uri = 'back/api/post';
       }
@@ -39,16 +40,24 @@ export default function Bubble(props) {
       try {
         const response = await fetch(uri);
         const data = await response.json();
-        setArr(data.data);
+
+        // Ensure data.data is an array
+        if (Array.isArray(data.data)) {
+          console.log(data.data);
+          setArr(data.data);
+        } else {
+          console.warn('Expected an array but got:', data.data);
+          setArr([]); // Set to empty array if data is not as expected
+        }
       } catch (error) {
         console.error('데이터를 가져오는 중 오류 발생:', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [userId]); // userId를 의존성 배열에 추가
 
-  const handleBubbleClick = (id) => {
+  const handleBubbleClick = id => {
     navigate(`/bubble/${id}`); // 클릭된 버블의 id를 포함하여 상세 페이지로 이동
   };
 
